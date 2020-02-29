4ff1c9f3d742106c8ebf1cd8986e6295b43fe761
kimberlyt <jkanjels@gmail.com>
2020-02-29 15:22:11 -0500

    'LAB5update4'

--- app/views.py
+++ app/views.py
@@ -28,6 +28,11 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    """Render a secure page on our website that only logged in users can access."""
+    return render_template('secure_page.html')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -56,7 +61,7 @@ def login():
 
                 # remember to flash a message to the user
                 flash('You have been logged in successfully.', 'success')
-                return redirect(url_for("secure-page"))  # they should be redirected to a secure-page route instead
+                return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
